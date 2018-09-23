3bf0ea2776043c9a040ba1ee8b8e45ffbfa018a3
seeseesky <ccskytpk@gmail.com>
2018-09-23 16:35:50 +0800

    finalize

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function main() {
 
 (function () {
    'use strict';
-   
+
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -17,7 +17,7 @@ function main() {
         }
       });
 
-	
+
     // Show Menu on Book
     $(window).bind('scroll', function() {
         var navHeight = $(window).height() - 500;
@@ -28,7 +28,7 @@ function main() {
         }
     });
 
-    $('body').scrollspy({ 
+    $('body').scrollspy({
         target: '.navbar-default',
         offset: 80
     });
@@ -41,8 +41,42 @@ function main() {
       $(".navbar-collapse").collapse('hide');
     }
   });
-	
-  	 
+
+  $('#askus').click(function(evt){
+    $('#askus').prop( "disabled", true );
+    $('#askus').html('正在送出訊息...');
+    evt.preventDefault();
+    var email = $('#email').val();
+    var name = $('#name').val();
+    var message = $('#message').val();
+    var templateParams = {
+      from_name: name,
+      from_email: email,
+      message_html: message
+    };
+
+    if(!email || !message) {
+      alert('請填寫電郵及訊息');
+      $('#askus').prop( "disabled", false );
+      $('#askus').html('送出訊息');
+      return;
+    }
+
+    emailjs.send('zoho', 'template_Ho1CodxE', templateParams)
+        .then(function(response) {
+           alert('感謝，訊息已送出，我們會盡快回覆你的查詢。');
+           $('#email').val('');
+           $('#name').val('');
+           $('#message').val('');
+           $('#askus').prop( "disabled", false );
+           $('#askus').html('送出訊息');
+        }, function(error) {
+           alert('無法送出訊息，請稍後再試或透過其他方式聯絡我們');
+           $('#askus').prop( "disabled", false );
+           $('#askus').html('送出訊息');
+        });
+  });
+
 
 }());
 
